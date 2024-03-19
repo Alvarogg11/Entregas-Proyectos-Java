be83be7dac7f83ab9f4b7bb7c87ad774e679f247
Alvarogg11 <gilgraciaalvaro@gmail.com>
2024-03-19 18:34:05 +0100

    Codigo completo proyecto 1

--- Laboratorio/Proyecto 1/fp/funciones/Funciones.java
+++ Laboratorio/Proyecto 1/fp/funciones/Funciones.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Funciones {
 	    
 	// FUNCIÓN 1
-	public static boolean numero_primo(int n) {
+	public static boolean numeroPrimo(int n) {
 	    if (n <= 1) {
 	        return false; // los números menores o iguales a 1 no son primos
 	    } else if (n == 2 || n == 3) {
@@ -25,42 +25,42 @@ public class Funciones {
 	    return true;
 	}
 	
-	// FUNCIÓN 2 (primero definimos la función factorial para implementarla en la función que nos piden)
-	public static Long factorial(int n) {
-	    Long result = 1L;
-	    for (int i = 1; i <= n; i++) {
-	        result *= i;
-	    }
-	    return result;
-	}
+    // FUNCIÓN 2 (primero definimos la función factorial para implementarla en la función que nos piden)
+    
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static int numeroCombinatorio(int n, int k) {
+        if (n < k) {
+            return 0; // n debe ser mayor o igual a k
+        } else {
+            int num = factorial(n); // n!
+            int den = factorial(k) * factorial(n - k); // k!*(n-k)!
+            int res = num / den;
+            return res;
+        }
+    }
 	
-	public static Long numero_combinatorio(int n, int k) {
-	    if (n < k) {
-	    	Integer temp;
-	    	temp = k;
-	    	k = n;
-	    	n=temp;
-	    }
-        Long num = factorial(n); // n!
-        Long den = factorial(k) * factorial(n - k); // k!*(n-k)!
-        Long res = num / den;
-        return  res;
-	}
 	
 	// FUNCIÓN 3
-	public static int calculo_s(int n, int k) {
-	    int i = 0;
-	    double sumatorio = 0;
-	    assert n >= k : "n debe ser mayor o igual que k";
-	    assert n >= i : "k debe ser mayor o igual que i";
-	    for (i = 0; i < k; i++) {
-	        sumatorio += Math.pow(-1, i) * numero_combinatorio(k, i)* Math.pow((k - i), n);
-	    }
-	    return (int) ((1.0 / factorial(k)) * sumatorio);
-	}
+    public static int calculoS(int n, int k) {
+        int i = 0;
+        double sumatorio = 0;
+        assert n >= k : "n debe ser mayor o igual que k";
+        assert n >= i : "k debe ser mayor o igual que i";
+        for (i = 0; i < k; i++) {
+            sumatorio += Math.pow(-1, i) * Integer.parseInt(Integer.toString(numeroCombinatorio(k, i))) * Math.pow((k - i), n);
+        }
+        return (int) ((1.0 / factorial(k)) * sumatorio);
+    }
 	
 	// FUNCIÓN 4
-	public static List<Integer> diferencias_numeros(List<Integer> numeros_enteros) {
+	public static List<Integer> diferenciasNumeros(List<Integer> numeros_enteros) {
 	    List<Integer> lista_diferencias_numeros = new ArrayList<>();
 	    for (int i = 1; i < numeros_enteros.size(); i++) {
 	        lista_diferencias_numeros.add(numeros_enteros.get(i) - numeros_enteros.get(i - 1));
@@ -69,7 +69,7 @@ public class Funciones {
 	}
 	
 	// FUNCIÓN 5
-	public static String cadena_mas_larga(List<String> lista_cadena) {
+	public static String cadenaMasLarga(List<String> lista_cadena) {
 	    String maxZ = null;
 	    for (String z : lista_cadena) {
 	        if (maxZ == null || z.length() > maxZ.length()) {
--- Laboratorio/Proyecto 1/fp/funciones/test/TestFunciones.java
+++ Laboratorio/Proyecto 1/fp/funciones/test/TestFunciones.java
@@ -1,21 +1,66 @@
 package fp.funciones.test;
 import fp.funciones.Funciones;
+import fp.funciones.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TestFunciones {
 	public static void main(String[] args) {
 		testNumeroPrimo();
+		testNumeroCombinatorio();
+		testCalculoS();
+		testDiferenciasNumeros();
+		testCadenaMasLarga();
+		
 	}
 public static void testNumeroPrimo() {
-	System.out.println("es "+Integer.toString(5)+" primo? "+Funciones.numero_primo(5));
-	System.out.println("es "+Integer.toString(2)+" primo? "+Funciones.numero_primo(2));
-	System.out.println("es "+Integer.toString(8)+" primo? "+Funciones.numero_primo(8));
-//en las siguientes funciones es lo mismo que lo anterior pero cambiando el final el 'Funciones.(método) cambiando ese método por el método correspondiente
+	System.out.println("Test función 1, Número Primo");
+	System.out.println("es "+Integer.toString(5)+" primo? "+Funciones.numeroPrimo(5));
+	System.out.println("es "+Integer.toString(2)+" primo? "+Funciones.numeroPrimo(2));
+	System.out.println("es "+Integer.toString(8)+" primo? "+Funciones.numeroPrimo(8));
+	System.out.println("                        ");
 	}
 
 public static void testNumeroCombinatorio() {
-	System.out.println("el número combinatorio "+Integer.toString(3)+" sobre "+Integer.toString(2)+" es: "+Funciones.numero_combinatorio(3, 2));
+	System.out.println("Test función 2, Número combinatorio");
+	System.out.println("El número combinatorio "+Integer.toString(3)+" sobre "+Integer.toString(2)+" es: "+Funciones.numeroCombinatorio(3, 2));
+	System.out.println("El número combinatorio "+Integer.toString(7)+" sobre "+Integer.toString(3)+" es: "+Funciones.numeroCombinatorio(7, 3));
+	System.out.println("El número combinatorio "+Integer.toString(9)+" sobre "+Integer.toString(5)+" es: "+Funciones.numeroCombinatorio(9, 5));
+	System.out.println("                        ");
+
 	}
+
 public static void testCalculoS() {
-	System.out.println("calculamos S(n,k) siendo n "+Integer.toString(0));
+	System.out.println("Test función 3, cálculo de S");
+	System.out.println("El cálculo de la función S para n = 5 y k = 2 es: "+Funciones.calculoS(5, 2));
+	System.out.println("El cálculo de la función S para n = 8 y k = 5 es: "+Funciones.calculoS(8, 5));
+	System.out.println("El cálculo de la función S para n = 20 y k = 11 es: "+Funciones.calculoS(20, 11));
+	System.out.println("                        ");
+
 	}
+
+
+public static void testDiferenciasNumeros() {
+	System.out.println("Test función 4, Diferencias entre números");
+	
+    List<Integer> numeros = new ArrayList<>();
+    numeros.add(1);
+    numeros.add(4);
+    numeros.add(7);
+    numeros.add(10);
+    System.out.println("La lista con las diferencias entre cada valor y el anterior es la siguiente: " + Funciones.diferenciasNumeros(numeros));
+	System.out.println("                        ");
+
+	}
+	
+public static void testCadenaMasLarga() {
+	System.out.println("Test función 5, Cadena más larga");
+	
+    List<String> listaCadena = new ArrayList<>();
+    listaCadena.add("Ordenador");
+    listaCadena.add("vaso");
+    listaCadena.add("Ingeniería");
+    System.out.println("La cadena más larga es: " +Funciones.cadenaMasLarga(listaCadena));
+	}
+	
 }
